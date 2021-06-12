5187ee387be098881220537d132a40b3cf7c9fbc
Rejaul-arim-shohag <jesminbegum5684@gmail.com>
2021-06-12 11:39:09 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrwHQmQUgfGEjGKLPirdHhMbHyV-sTS-0",
+    authDomain: "burj-al-arab-9e967.firebaseapp.com",
+    projectId: "burj-al-arab-9e967",
+    storageBucket: "burj-al-arab-9e967.appspot.com",
+    messagingSenderId: "388238117983",
+    appId: "1:388238117983:web:6fb1d322969f21cf511fea"
+  };
+  export default firebaseConfig;
